ce7c4d20c9ca3db3bdae937e146fb0fd8be4f466
arifiani1009 <aifiani.tc09@gmail.com>
2020-05-05 10:00:35 +0700

    Add all my files

---
+++ src/components/layout/SidebarMenu.vue
@@ -0,0 +1,63 @@
+<template>
+  <ul class="sidebar-menu">
+    <li class="header">TOOLS</li>
+    <router-link tag="li" class="pageLink" to="/">
+      <a>
+        <i class="fa fa-desktop"></i>
+        <span class="page">Dashboard</span>
+      </a>
+    </router-link>
+    <router-link tag="li" class="pageLink" to="/tables">
+      <a>
+        <i class="fa fa-table"></i>
+        <span class="page">Tabel Transaksi</span>
+      </a>
+    </router-link>
+	<router-link tag="li" class="pageLink" to="/tables">
+      <a>
+        <i class="fa fa-table"></i>
+        <span class="page">Tabel Produk</span>
+      </a>
+    </router-link>
+	<router-link tag="li" class="pageLink" to="/tables">
+      <a>
+        <i class="fa fa-table"></i>
+        <span class="page">Tabel User</span>
+      </a>
+    </router-link>
+
+    
+  </ul>
+</template>
+<script>
+export default {
+  name: 'SidebarMenu'
+}
+</script>
+<style>
+/* override default */
+.sidebar-menu > li > a {
+  padding: 12px 15px 12px 15px;
+}
+
+.sidebar-menu li.active > a > .fa-angle-left,
+.sidebar-menu li.active > a > .pull-right-container > .fa-angle-left {
+  animation-name: rotate;
+  animation-duration: 0.2s;
+  animation-fill-mode: forwards;
+}
+
+.treeview-title {
+  z-index: 1;
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(-90deg);
+  }
+}
+</style>
